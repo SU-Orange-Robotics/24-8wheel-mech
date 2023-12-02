3ea2bd927160096a825dc08b3f4863a965aec06e
Andrew Tedesco <davethecubebot@gmail.com>
2023-12-02 18:48:05 -0500

    at comp updates

--- src/intakeCat.cpp
+++ src/intakeCat.cpp
@@ -4,6 +4,8 @@
 
 using namespace vex;
 
+bool autoArming = false;
+
 void intakeSpin(bool reversed) {
     intake.spin(directionType::fwd, intakePow * (reversed ? -1 : 1), percentUnits::pct);
 }
@@ -12,7 +14,7 @@ void intakeStop() {
     intake.stop();
 }
 
-void catapultArm() {
+void catapultLower() {
     catapultA.spin(directionType::fwd, catPow, percentUnits::pct);
     catapultB.spin(directionType::fwd, catPow, percentUnits::pct);
     catapultC.spin(directionType::fwd, catPow, percentUnits::pct);
@@ -22,4 +24,41 @@ void catapultStop() {
     catapultA.stop();
     catapultB.stop();
     catapultC.stop();
+}
+
+void catapultArm() {
+    autoArming = true;
+    catapultLower();
+    waitUntil(catInPosArmed() || !autoArming);
+    catapultStop();
+    autoArming = false;
+}
+
+void catapultLaunch() {
+    autoArming = true;
+    catapultLower();
+    waitUntil(catInPosShoot() || !autoArming);
+    catapultStop();
+}
+
+bool catInPosArmed() {
+    double posA1 = 197, posA2 = 200;//197.57, 197.75
+
+    double currPos = catapultRot.angle(rotationUnits::deg);
+    //modf(360, &currPos);
+
+    return ((currPos > posA1 && currPos < posA2) /*|| (currPos > posB1 && currPos < posB2)*/);
+}
+
+bool catInPosShoot() {
+    double posA1 = 248, posA2 = 255;
+
+    double currPos = catapultRot.angle(rotationUnits::deg);
+    //modf(360, &currPos);
+
+    return (currPos > posA1 && currPos < posA2);
+}
+
+void stopAutoArming() {
+    autoArming = false;
 }
